44e6a74fb3e4ca781c130e3038c5314bb6a0301f
dealerdenesquik <antoninelne@gmail.com>
2016-12-13 23:35:04 +0100

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <termios.h>
 
-//Pour avoir accès à ce fichier merci de bien vouloir contacter le 06.45.27.82.10 pour les modalitées de paiement
-// Ce dernier contient toute les fonction pour le projet fini
-
-
+//Pour avoir accès à tout les fichiers du projet finalisé merci de bien vouloir contacter le 06.45.27.82.10 pour les modalitées de paiment
+//Appel gratuit depuis un poste fixe 
+// Prix d'un appel local 
+// SMS gratuit depuis un terminal compatible
